c90713f4d9d524913aff16e3b063954c492b2bf9
alfatech.uz <golibjonodinaev@gmail.com>
2024-03-13 13:30:41 +0500

    Vtorou etap1

--- app/src/main/java/com/example/stringgrid/MainActivity.java
+++ app/src/main/java/com/example/stringgrid/MainActivity.java
@@ -5,13 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 
 public class MainActivity extends AppCompatActivity {
 //  @Override
@@ -35,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         simpleGrid = (GridView) findViewById(R.id.simpleGridView); // init GridView
         // Create an object of CustomAdapter and set Adapter to GirdView
-        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), logos);
+        CustomAdapter customAdapter;
+        customAdapter = new CustomAdapter(getApplicationContext(), logos);
         simpleGrid.setAdapter(customAdapter);
         // implement setOnItemClickListener event on GridView
         simpleGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
