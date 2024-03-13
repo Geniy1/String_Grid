7356940c5c661801de9c8620be84d0a1a5849990
alfatech.uz <golibjonodinaev@gmail.com>
2024-03-13 13:01:27 +0500

    Vtorou etap

---
+++ app/src/main/java/com/example/stringgrid/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.stringgrid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+//  @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+//    }
+    GridView simpleGrid;
+    int logos[] = {R.drawable.logo1, R.drawable.logo2, R.drawable.logo3, R.drawable.logo4,
+            R.drawable.logo5, R.drawable.logo6, R.drawable.logo7, R.drawable.logo8, R.drawable.logo9,
+            R.drawable.logo10};
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        simpleGrid = (GridView) findViewById(R.id.simpleGridView); // init GridView
+        // Create an object of CustomAdapter and set Adapter to GirdView
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), logos);
+        simpleGrid.setAdapter(customAdapter);
+        // implement setOnItemClickListener event on GridView
+        simpleGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // set an Intent to Another Activity
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("image", logos[position]); // put image data in Intent
+                startActivity(intent); // start Intent
+            }
+        });
+    }
+
+}
